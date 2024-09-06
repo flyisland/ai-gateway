dab2bd3c513d36041a3e76aabb1bb004de89fe7a
Anatoli Babenia <anatoli@rainforce.org>
2024-09-06 15:59:57 +0300

    feat: show filenames in title diff test
    The `jq` query transforms this structure:
    
    {
      content: ...
      metadata: {
        title: ...
        source: ...
      }
    }
    
    into this
    
    [
      {
        source: ...
        titles:
          [
            ...,
            ...,
            ...
          ]
        ]
      }
    ]

---
+++ scripts/ingest/gitlab-docs/test_titles.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -eu
+
+FIRST=/tmp/ruby.jsonl
+SECOND=/tmp/python.jsonl
+
+echo "--- Extracting titles grouped under filename"
+
+jq --slurp 'group_by(.metadata.source) | map({source: .[0].metadata.source, titles: [map(.metadata.title)]})' < "$FIRST" > "$FIRST.titles"
+
+jq --slurp 'group_by(.metadata.source) | map({source: .[0].metadata.source, titles: [map(.metadata.title)]})' < "$SECOND" > "$SECOND.titles"
+
+diff -u3 "$FIRST.titles" "$SECOND.titles"
