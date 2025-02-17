7025d93106c7dc76f37e01443024117c7ed7c0dd
Igor Drozdov <idrozdov@gitlab.com>
2025-02-17 15:00:44 +0100

    feat(q): accept system-hook source as event payload

--- ai_gateway/api/v1/amazon_q/typing.py
+++ ai_gateway/api/v1/amazon_q/typing.py
@@ -1,4 +1,4 @@
-from typing import Annotated, List, Literal, Optional, Union
+from typing import Annotated, Any, List, Literal, Optional, Union
 
 from pydantic import BaseModel, Field, StringConstraints
 
@@ -50,11 +50,16 @@ class EventIssuePayload(EventRequestPayload):
     issue_iid: Annotated[str, StringConstraints(max_length=1024)]
 
 
+class EventHookPayload(BaseModel):
+    source: Literal["system_hook"]
+    data: dict[str, Any]
+
+
 class EventRequest(BaseModel):
     role_arn: Annotated[str, StringConstraints(max_length=2048)]
     code: Annotated[str, StringConstraints(max_length=255)]
-    payload: Union[EventMergeRequestPayload, EventIssuePayload] = Field(
-        discriminator="source"
+    payload: Union[EventMergeRequestPayload, EventIssuePayload, EventHookPayload] = (
+        Field(discriminator="source")
     )
 
 
