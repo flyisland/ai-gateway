85d96c07e9103acbc9e2bec31606ea1449d59ad9
Eduardo Bonet <ebonet@gitlab.com>
2025-03-31 11:26:56 +0200

    chore: apply suggestions
    Co-authored-by: Tan Le <tle@gitlab.com>

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,10 +1,10 @@
 module.exports = {
-    extends: ['@commitlint/config-conventional'],
-    rules: {
-        "subject-case": [
-            0,
-            "always",
-            "lowerCase"
-        ],
-    },
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    "subject-case": [
+      0,
+      "always",
+      "lowerCase"
+    ],
+  },
 };
