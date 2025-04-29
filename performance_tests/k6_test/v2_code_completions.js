2c34ac6c9fc0d156a0c2157d21e17e26027dff28
vishal.s.patel <vpatel@gitlab.com>
2025-04-29 16:49:47 +1200

    chore: adding scenarios
    adding warmup period to test

--- performance_tests/k6_test/v2_code_completions.js
+++ performance_tests/k6_test/v2_code_completions.js
@@ -6,8 +6,21 @@ export const RPS_THRESHOLD= 2;
 export const TEST_NAME='v2_code_completions'
 
 export const options = {
-  vus: 2, // Number of virtual users
-  duration: '60s', // Test duration
+  scenarios: {
+    warmup: {
+      executor: 'constant-vus',
+      vus: 1,
+      duration: '10s',
+      gracefulStop: '0s',
+      tags: { scenario: 'warmup' }, // Tag these requests to filter them out
+    },
+    load_test: {
+      executor: 'constant-vus',
+      vus: 2,
+      duration: '50s',
+      startTime: '10s', // Start after warmup completes
+      tags: { scenario: 'load_test' },
+    },
 };
 
 export default function () {
