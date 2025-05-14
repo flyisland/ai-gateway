0b4c438d24d7ffbea265ef71ab981927e30880d7
AndrasHerczeg <aherczeg@gitlab.com>
2025-05-14 10:20:21 +0200

    feat: Add the correlation ID middleware to the websocket server
    Translate the CorrelationIdInterceptor logic from the gRPC server into a FastAPI middleware format

--- duo_workflow_service/interceptors/correlation_id_interceptor.py
+++ duo_workflow_service/interceptors/correlation_id_interceptor.py
@@ -2,12 +2,18 @@ import uuid
 from contextvars import ContextVar
 
 import grpc
+from fastapi import WebSocket
 
-# Context variable to store correlation ID
+from duo_workflow_service.interceptors.websocket_middleware import WebSocketMiddleware
+
+# Context variables to store correlation ID and user ID
 correlation_id: ContextVar[str] = ContextVar("correlation_id", default="undefined")
 gitlab_global_user_id: ContextVar[str] = ContextVar(
     "gitlab_global_user_id", default="undefined"
 )
+
+CORRELATION_ID_KEY = "x-request-id"
+X_GITLAB_GLOBAL_USER_ID_HEADER = "x-gitlab-global-user-id"
 
 
 class CorrelationIdInterceptor(grpc.aio.ServerInterceptor):
@@ -28,12 +34,33 @@ class CorrelationIdInterceptor(grpc.aio.ServerInterceptor):
         metadata = dict(handler_call_details.invocation_metadata)
 
         # Extract correlation ID from metadata or generate new one
-        request_id = metadata.get(self.CORRELATION_ID_KEY, str(uuid.uuid4()))
+        request_id = metadata.get(CORRELATION_ID_KEY, str(uuid.uuid4()))
 
         # Set correlation ID in context
         correlation_id.set(request_id)
         gitlab_global_user_id.set(
-            metadata.get(self.X_GITLAB_GLOBAL_USER_ID_HEADER, "undefined")
+            metadata.get(X_GITLAB_GLOBAL_USER_ID_HEADER, "undefined")
         )
 
         return await continuation(handler_call_details)
+
+
+class CorrelationIdMiddleware(WebSocketMiddleware):
+    """Middleware for handling correlation IDs."""
+
+    async def __call__(
+        self,
+        websocket: WebSocket,
+    ):
+        request_id = websocket.headers.get(CORRELATION_ID_KEY)
+        user_id = websocket.headers.get(X_GITLAB_GLOBAL_USER_ID_HEADER)
+
+        if request_id is None:
+            request_id = str(uuid.uuid4())
+
+        if user_id is None:
+            user_id = "undefined"
+
+        # Set correlation ID in context
+        correlation_id.set(request_id)
+        gitlab_global_user_id.set(user_id)
