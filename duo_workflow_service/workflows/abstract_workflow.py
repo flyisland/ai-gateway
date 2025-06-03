22189da6b65f16c5e4a78cad212f09dda36b653e
Joey_Khabie <jkhabie@gitlab.com>
2025-06-03 13:11:56 +0300

    chore: Fix linting - part 3

--- duo_workflow_service/workflows/abstract_workflow.py
+++ duo_workflow_service/workflows/abstract_workflow.py
@@ -6,6 +6,7 @@ from typing import Any, Dict, Optional, Type, TypedDict, Union
 import structlog
 from langchain.tools import BaseTool
 from langchain_core.runnables import RunnableConfig
+
 # pylint disable are going to be fixed via
 # https://gitlab.com/gitlab-org/duo-workflow/duo-workflow-service/-/issues/78
 from langgraph.checkpoint.base import (  # pylint: disable=no-langgraph-langchain-imports
