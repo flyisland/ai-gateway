62e01524ebf9cfa229e519748edc0e1d2b505281
Eva Kadlecová <ekadlecova@gitlab.com>
2025-06-27 16:31:06 +0200

    feat: update indentation in query

--- duo_workflow_service/tools/queries/epics.py
+++ duo_workflow_service/tools/queries/epics.py
@@ -1,22 +1,22 @@
 GET_EPIC_NOTES_QUERY = """
     query GetEpicNotes($fullPath: ID!, $epicIid: String!) {
-      namespace(fullPath: $fullPath) {
-        workItem(iid: $epicIid) {
-          widgets {
-            ... on WorkItemWidgetNotes {
-              notes {
-                nodes {
-                  id
-                  body
-                  createdAt
-                  author {
-                    username
-                  }
+        namespace(fullPath: $fullPath) {
+            workItem(iid: $epicIid) {
+                widgets {
+                    ... on WorkItemWidgetNotes {
+                        notes {
+                            nodes {
+                                id
+                                body
+                                createdAt
+                                author {
+                                    username
+                                }
+                            }
+                        }
+                    }
                 }
-              }
             }
-          }
         }
-      }
     }
     """
