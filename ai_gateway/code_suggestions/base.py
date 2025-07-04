bf4f80beea0927b9a7baeac4393da4fa1b607375
pkanellidis <pkanellidis@gitlab.com>
2025-07-04 10:46:52 +0300

    chore: Make RCA Claude 4 the default

--- ai_gateway/code_suggestions/base.py
+++ ai_gateway/code_suggestions/base.py
@@ -112,6 +112,10 @@ SAAS_PROMPT_MODEL_MAP = {
         "model_provider": ModelProvider.ANTHROPIC,
         "model_version": KindAnthropicModel.CLAUDE_SONNET_4,
     },
+    "1.1.0": {
+        "model_provider": ModelProvider.ANTHROPIC,
+        "model_version": KindAnthropicModel.CLAUDE_SONNET_4,
+    },
     "3.0.2-dev": {
         "model_provider": ModelProvider.ANTHROPIC,
         "model_version": KindAnthropicModel.CLAUDE_3_7_SONNET,
