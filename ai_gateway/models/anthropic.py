ab1f9f4e4d155f236484e288cb059f3a948f1196
Nathan Weinshenker <nweinshenker@gitlab.com>
2025-07-09 12:31:46 -0600

    fix: make sure proper langchain-anthropic package is add
    the following 0.3.17 package is required to ensure the beta
    param for ChatAnthropic is added

--- ai_gateway/models/anthropic.py
+++ ai_gateway/models/anthropic.py
@@ -80,7 +80,6 @@ class KindAnthropicModel(StrEnum):
     CLAUDE_3_7_SONNET_VERTEX = "claude-3-7-sonnet@20250219"
     CLAUDE_SONNET_4 = "claude-sonnet-4-20250514"
     CLAUDE_SONNET_4_VERTEX = "claude-sonnet-4@20250514"
-    CLAUDE_SONNET_4_OPUS = "claude-opus-4-20250514"
 
 
 class AnthropicModel(TextGenModelBase):
