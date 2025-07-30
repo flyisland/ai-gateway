a1d072ee6ae184c4b1d34763cba4e8983c16b831
Missy Davies <mdavies@gitlab.com>
2025-07-30 14:33:12 -0400

    feat: refactor so code completion uses prompt registry

--- ai_gateway/api/v2/code/completions.py
+++ ai_gateway/api/v2/code/completions.py
@@ -450,32 +450,23 @@ def _resolve_code_completions_litellm(
     payload: SuggestionsRequest,
     current_user: StarletteUser,
     prompt_registry: BasePromptRegistry,
-    use_llm_prompt_caching: bool,
+    use_llm_prompt_caching: bool, # pylint: disable=unused-argument
     completions_agent_factory: Factory[CodeCompletions],
-    completions_litellm_factory: Factory[CodeCompletions],
+    completions_litellm_factory: Factory[CodeCompletions], # pylint: disable=unused-argument
 ) -> CodeCompletions:
-    if payload.prompt_version == 2 and not payload.prompt:
-        model_metadata = ModelMetadata(
-            name=payload.model_name,
-            endpoint=payload.model_endpoint,
-            api_key=payload.model_api_key,
-            identifier=payload.model_identifier,
-            provider=payload.model_provider or "text-completion-openai",
-        )
+    model_metadata = ModelMetadata(
+        name=payload.model_name,
+        endpoint=payload.model_endpoint,
+        api_key=payload.model_api_key,
+        identifier=payload.model_identifier,
+        provider=payload.model_provider or "text-completion-openai",
+    )
 
-        return _resolve_agent_code_completions(
-            model_metadata=model_metadata,
-            current_user=current_user,
-            prompt_registry=prompt_registry,
-            completions_agent_factory=completions_agent_factory,
-        )
-
-    return completions_litellm_factory(
-        model__name=payload.model_name,
-        model__endpoint=payload.model_endpoint,
-        model__api_key=payload.model_api_key,
-        model__provider=payload.model_provider,
-        model__using_cache=use_llm_prompt_caching,
+    return _resolve_agent_code_completions(
+        model_metadata=model_metadata,
+        current_user=current_user,
+        prompt_registry=prompt_registry,
+        completions_agent_factory=completions_agent_factory,
     )
 
 
