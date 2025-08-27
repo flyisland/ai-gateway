79c31a2f81a00c094cec4c79f3a6b480cca7e4ed
flux423 <admin@bluefly.io>
2025-08-27 13:05:30 -0400

    fix: update Jest config for ES modules and fix dependencies

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+export default {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/src', '<rootDir>/tests'],
+  testMatch: ['**/__tests__/**/*.ts', '**/?(*.)+(spec|test).ts'],
+  testPathIgnorePatterns: ['<rootDir>/tests/playwright/'],
+  transform: {
+    '^.+\\.ts$': ['ts-jest', {
+      useESM: true,
+    }],
+  },
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!src/**/*.d.ts',
+  ],
+  extensionsToTreatAsEsm: ['.ts'],
+};
