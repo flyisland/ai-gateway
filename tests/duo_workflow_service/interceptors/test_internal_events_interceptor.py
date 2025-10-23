88a620b4b3797e4a8fd6b9c41a49d69a36024d06
Joey_Khabie <jkhabie@gitlab.com>
2025-10-23 10:59:22 +0300

    chore: Address code review

--- tests/duo_workflow_service/interceptors/test_internal_events_interceptor.py
+++ tests/duo_workflow_service/interceptors/test_internal_events_interceptor.py
@@ -7,6 +7,7 @@ from duo_workflow_service.interceptors.gitlab_version_interceptor import gitlab_
 from duo_workflow_service.interceptors.internal_events_interceptor import (
     InternalEventsInterceptor,
 )
+from duo_workflow_service.interceptors.gitlab_version_interceptor import gitlab_version
 from duo_workflow_service.interceptors.language_server_version_interceptor import (
     language_server_version,
 )
